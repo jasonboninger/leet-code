ccc45c0813fd8d0016abe7446232f2cec67ab01b
Jason Boninger <jason.boninger@gmail.com>
2021-07-13 21:16:37 -0700

    Added solution to add two numbers.

--- _Workbench.cs
+++ _Workbench.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution
 {
-    public int[] TwoSum(int[] nums, int target)
+    private static readonly ListNode _zero = new ListNode() { next = _zero };
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        var numNeededToIndexMappings = new Dictionary<int, int>();
-		for (int i = 0; i < nums.Length; i++)
-		{
-            var num = nums[i];
-            if (numNeededToIndexMappings.TryGetValue(num, out var index))
-			{
-                return new int[] { index, i };
-			}
-            numNeededToIndexMappings[target - num] = i;
-		}
-        throw new InvalidOperationException("No two numbers sum to target.");
+        var first = new ListNode();
+        _AddTwoNumbers(first, l1, l2);
+        return first;
+    }
+
+    private static void _AddTwoNumbers(ListNode current, ListNode one, ListNode two)
+    {
+        current.val += one.val + two.val;
+        int carry;
+        if (current.val >= 10)
+        {
+            current.val -= 10;
+            carry = 1;
+        }
+        else
+        {
+            carry = 0;
+        }
+        one = one.next ?? _zero;
+        two = two.next ?? _zero;
+        if (one != _zero || two != _zero || carry != 0)
+        {
+            current.next = new ListNode(carry);
+            _AddTwoNumbers(current.next, one, two);
+        }
     }
 }
