78b1fb33b6baa6a8bcd911702153eec6d931c959
Jason Boninger <jason.boninger@gmail.com>
2021-07-14 22:55:42 -0700

    Added solution.

---
+++ LeetCode/Problem3LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public static class Problem3LongestSubstringWithoutRepeatingCharacters
+	{
+		private class DistinctQueue<TValue>
+		{
+			public int Count => _queue.Count;
+
+			private readonly HashSet<TValue> _hashSet = new();
+			private readonly Queue<TValue> _queue = new();
+
+			public bool Enqueue(TValue value)
+			{
+				if (_hashSet.Add(value))
+				{
+					_queue.Enqueue(value);
+					return true;
+				}
+				return false;
+			}
+
+			public bool TryDequeue(out TValue value)
+			{
+				if (_queue.TryDequeue(out value))
+				{
+					_hashSet.Remove(value);
+					return true;
+				}
+				return false;
+			}
+		}
+
+		public static int LengthOfLongestSubstring(string s)
+		{
+			var run = new DistinctQueue<char>();
+			var best = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				var character = s[i];
+				if (!run.Enqueue(character))
+				{
+					best = Math.Max(best, run.Count);
+					while (run.TryDequeue(out var characterExisting))
+					{
+						if (characterExisting == character)
+						{
+							break;
+						}
+					}
+				}
+				run.Enqueue(character);
+			}
+			return Math.Max(best, run.Count);
+		}
+	}
+}
