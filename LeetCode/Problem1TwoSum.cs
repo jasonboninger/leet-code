314943f4aca9e4bd39027d68154b2e6a921ce812
Jason Boninger <jboninger@insiten.com>
2021-07-14 20:04:37 -0700

    Converted to solution.

---
+++ LeetCode/Problem1TwoSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public static class Problem1TwoSum
+	{
+		public static int[] TwoSum(int[] nums, int target)
+		{
+			var numNeededToIndexMappings = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				var num = nums[i];
+				if (numNeededToIndexMappings.TryGetValue(num, out var index))
+				{
+					return new int[] { index, i };
+				}
+				numNeededToIndexMappings[target - num] = i;
+			}
+			throw new InvalidOperationException("No two numbers sum to target.");
+		}
+	}
+}
---
+++ LeetCode/Problem2AddTwoNumbers.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode
+{
+    public static class Problem2AddTwoNumbers
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        private static readonly ListNode _zero = new();
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var first = new ListNode();
+            _AddTwoNumbers(first, l1, l2);
+            return first;
+        }
+
+        private static void _AddTwoNumbers(ListNode current, ListNode one, ListNode two)
+        {
+            current.val += one.val + two.val;
+            int carry;
+            if (current.val >= 10)
+            {
+                current.val -= 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+            one = one.next ?? _zero;
+            two = two.next ?? _zero;
+            if (one != _zero || two != _zero || carry != 0)
+            {
+                current.next = new(carry);
+                _AddTwoNumbers(current.next, one, two);
+            }
+        }
+    }
+}
