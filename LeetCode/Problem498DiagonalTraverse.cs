43e0b32379cb7eca950ba775fdd67633b054150a
Jason Boninger <jason.boninger@gmail.com>
2021-07-18 20:20:59 -0700

    Added solution.

---
+++ LeetCode/Problem498DiagonalTraverse.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode
+{
+	internal static class Problem498DiagonalTraverse
+	{
+		internal static int[] FindDiagonalOrder(int[][] mat)
+		{
+			var indexReverseHorizontal = mat[0].Length - 1;
+			var indexReverseVertical = mat.Length - 1;
+			var lengthTotal = (indexReverseHorizontal + 1) * (indexReverseVertical + 1);
+			var indexHorizontal = 0;
+			var indexVertical = 0;
+			var indexTotal = 0;
+			var rising = true;
+			var values = new int[lengthTotal];
+			while (true)
+			{
+				values[indexTotal++] = mat[indexVertical][indexHorizontal];
+				if (indexTotal == lengthTotal)
+				{
+					return values;
+				}
+				if (rising)
+				{
+					if (indexHorizontal == indexReverseHorizontal)
+					{
+						indexVertical++;
+						rising = false;
+					}
+					else if (indexVertical == 0)
+					{
+						indexHorizontal++;
+						rising = false;
+					}
+					else
+					{
+						indexHorizontal++;
+						indexVertical--;
+					}
+				}
+				else
+				{
+					if (indexVertical == indexReverseVertical)
+					{
+						indexHorizontal++;
+						rising = true;
+					}
+					else if (indexHorizontal == 0)
+					{
+						indexVertical++;
+						rising = true;
+					}
+					else
+					{
+						indexHorizontal--;
+						indexVertical++;
+					}
+				}
+			}
+		}
+	}
+}
