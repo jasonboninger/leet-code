d5b6b0372307471d874bd2a6c8b744b780bdda13
Jason Boninger <jason.boninger@gmail.com>
2021-07-20 00:05:26 -0700

    Added solution.

---
+++ LeetCode/Problem10RegularExpressionMatching.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode
+{
+	internal static class Problem10RegularExpressionMatching
+	{
+		internal static bool IsMatch(string s, string p)
+		{
+			return _IsMatch(s, 0, p, 0);
+		}
+
+		private static bool _IsMatch(string @string, int indexString, string expression, int indexExpression)
+		{
+			if (indexString == @string.Length)
+			{
+				if (indexExpression == expression.Length)
+				{
+					return true;
+				}
+				var optionals = 0;
+				while (optionals < expression.Length && expression[^(optionals + 1)] == '*')
+				{
+					optionals += 2;
+				}
+				return indexExpression >= expression.Length - optionals;
+			}
+			if (indexExpression == expression.Length)
+			{
+				return false;
+			}
+			var characterString = @string[indexString];
+			var characterExpression = expression[indexExpression];
+			if 
+			(
+				(
+					characterString == characterExpression
+					|| characterExpression == '.'
+				)
+				&& _IsMatch(@string, indexString + 1, expression, indexExpression + 1)
+			)
+			{
+				return true;
+			}
+			if
+			(
+				characterExpression == '*'
+				&& _IsMatch(@string, indexString, expression, indexExpression - 1)
+			)
+			{
+				return true;
+			}
+			if
+			(
+				indexExpression < expression.Length - 1
+				&& expression[indexExpression + 1] == '*'
+				&& _IsMatch(@string, indexString, expression, indexExpression + 2)
+			)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
