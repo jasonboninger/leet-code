f9b47b6fab2b5b34ce96bf5ebaab4fa49f152f72
Jason Boninger <jason.boninger@gmail.com>
2021-07-20 22:57:30 -0700

    Added more readable solution.

--- LeetCode/Problem10RegularExpressionMatching.cs
+++ LeetCode/Problem10RegularExpressionMatching.cs
@@ -4,59 +4,69 @@
 	{
 		internal static bool IsMatch(string s, string p)
 		{
-			return _IsMatch(s, 0, p, 0);
+			return _IsMatch(s, 0, _GetExpression(p), 0);
 		}
 
-		private static bool _IsMatch(string @string, int indexString, string expression, int indexExpression)
+		private static bool _IsMatch(string @string, int indexString, (char character, bool zeroOrMore)[] expression, int indexExpression)
 		{
 			if (indexString == @string.Length)
 			{
-				if (indexExpression == expression.Length)
+				while (indexExpression < expression.Length)
 				{
-					return true;
+					if (!expression[indexExpression++].zeroOrMore)
+					{
+						return false;
+					}
 				}
-				var optionals = 0;
-				while (optionals < expression.Length && expression[^(optionals + 1)] == '*')
-				{
-					optionals += 2;
-				}
-				return indexExpression >= expression.Length - optionals;
+				return true;
 			}
 			if (indexExpression == expression.Length)
 			{
 				return false;
 			}
 			var characterString = @string[indexString];
-			var characterExpression = expression[indexExpression];
-			if 
-			(
-				(
-					characterString == characterExpression
-					|| characterExpression == '.'
-				)
-				&& _IsMatch(@string, indexString + 1, expression, indexExpression + 1)
-			)
+			var (characterExpression, zeroOrMore) = expression[indexExpression];
+			var match = characterExpression == characterString || characterExpression == '.';
+			if (zeroOrMore)
 			{
-				return true;
+				if (match && _IsMatch(@string, indexString + 1, expression, indexExpression))
+				{
+					return true;
+				}
+				return _IsMatch(@string, indexString, expression, indexExpression + 1);
 			}
-			if
-			(
-				characterExpression == '*'
-				&& _IsMatch(@string, indexString, expression, indexExpression - 1)
-			)
+			if (match)
 			{
-				return true;
-			}
-			if
-			(
-				indexExpression < expression.Length - 1
-				&& expression[indexExpression + 1] == '*'
-				&& _IsMatch(@string, indexString, expression, indexExpression + 2)
-			)
-			{
-				return true;
+				return _IsMatch(@string, indexString + 1, expression, indexExpression + 1);
 			}
 			return false;
+		}
+
+		private static (char character, bool zeroOrMore)[] _GetExpression(string expression)
+		{
+			var zeroOrMoreCharactersCount = 0;
+			for (int i = 0; i < expression.Length; i++)
+			{
+				if (expression[i] == '*')
+				{
+					zeroOrMoreCharactersCount++;
+				}
+			}
+			var expressionCharacters = new (char character, bool zeroOrMore)[expression.Length - zeroOrMoreCharactersCount];
+			var expressionCharactersIndex = 0;
+			for (int i = 0; i < expression.Length; i++)
+			{
+				var character = expression[i];
+				if (character == '*')
+				{
+					expressionCharacters[expressionCharactersIndex - 1].zeroOrMore = true;
+				}
+				else
+				{
+					expressionCharacters[expressionCharactersIndex++] = (character, zeroOrMore: false);
+				}
+			}
+			return expressionCharacters;
 		}
 	}
 }
